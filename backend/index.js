e06ba61d253fd9f5571aabdd76fb78ebec3f38a2
Kiliman-Jaro <tothkilian777@gmail.com>
2023-11-25 17:18:24 +0100

    backend legenerálása

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+
+
+const app = express()
+
+app.use(cors({ origin: true, credentials: true }))
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(5000, () => console.log(`Server started on port: 5000`))
