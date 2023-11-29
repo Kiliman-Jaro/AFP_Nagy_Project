c0ecc966cd060c55f315a488ab1b90ae23383098
Kiliman-Jaro <tothkilian777@gmail.com>
2023-11-29 16:36:31 +0100

    bejelentkezés frontend elkezdése

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+        if(!username || !password){
+            alert("Minden mezőt ki kell tölteni!");
+            return;
+        }
+        onLogin({username, password});
+    };
+
+    return (
+        <div className="login-form">
+            <input
+                type="text"
+                placeholder="Felhasználónév"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Jelszó"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Bejelentkezés</button>
+        </div>
+    );
+};
+export default LoginForm;
