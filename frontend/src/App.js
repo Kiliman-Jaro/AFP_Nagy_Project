b4baf681a3aa98ec94f50f4fbe471f85c433be17
Kiliman-Jaro <tothkilian777@gmail.com>
2023-12-06 18:02:15 +0100

    Home oldal lére hozása

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import RegisterForm from './components/RegisterForm.js';
 import LoginForm from './components/LoginForm.js';
+import Home from './components/Home.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/register" element={<RegisterForm />} />
+               < Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
   );
