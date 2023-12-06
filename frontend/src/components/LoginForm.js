00135839f02f3cddcd1e810a220e5cfde9e98a87
Kiliman-Jaro <tothkilian777@gmail.com>
2023-12-06 18:03:45 +0100

    login frissítése

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         if(!username || !password){
             alert("Minden mezőt ki kell tölteni!");
             return;
         }
+        axios.post("http://localhost:5000/login", {
+            username: username,
+            password: password,
+        }).then((response) => {
+            if(response.status === 200){
+                navigate("/home");
+            }
+            else{
+                alert("Sikertelen bejelentkezés!");
+                setUsername("");
+                setPassword("");
+            }
+        });
     };
 
     return (
