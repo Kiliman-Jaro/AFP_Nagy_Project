7a05286ef8044be43f594a9f9d67bfae23fc91ff
Kiliman-Jaro <tothkilian777@gmail.com>
2023-12-12 19:03:21 +0100

    Home.js átdolgozása
    Co-Authored-By: Bóta Dániel <145868940+Danszi44@users.noreply.github.com>

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,101 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+  const [pizzas, setPizzas] = useState([])
+  const [basket, setBasket] = useState([])
+  const [basketOpen, setBasketOpen] = useState(false)
+  const navigate = useNavigate()
+  function addToBasket(pizza) {
+    setBasket([...basket, pizza])
+  }
+  function seeBusket() {
+    setBasketOpen(true)
+  }
+  function closeBasket() {
+    setBasketOpen(false)
+  }
+  function remo(pizza) {
+    basket.splice(basket.indexOf(pizza), 1)
+    setBasket([...basket])
+  }
+  function logout() {
+    navigate("/login")
+  }
+  useEffect(() => {
+   setPizzas([
+    {
+      name: "Margherita",
+      ingredients: "Tomato, mozzarella, basil",
+      price: 12.99,
+      image: "pizza1.jpg"
+    },
+    {
+      name: "Pepperoni",
+      ingredients: "Pepperoni, tomato sauce, mozzarella",
+      price: 14.99,
+      image: "pizza1.jpg"
+    },
+    {
+      name: "asd",
+      ingredients: "Tomato, mozzarella, basil",
+      price: 12.99,
+      image: "pizza1.jpg"
+    },
+    {
+      name: "asd2",
+      ingredients: "Tomato, mozzarella, basil",
+      price: 12.99,
+      image: "pizza1.jpg"
+    },
+    {
+      name: "asd3",
+      ingredients: "Tomato, mozzarella, basil",
+      price: 12.99,
+      image: "pizza1.jpg"
+    }
+   ])
+  }, [])
   return (
-    <div>Home</div>
+    <div>
+      <header>
+        <div class="title">Pizzeria Online Shop</div>
+       <div id="basket-count">{basket.length}</div>
+        <div id="basket-icon" onClick={seeBusket}>&#128722;</div>
+        <button onClick={closeBasket}> All pizzas</button>
+        <button onClick={logout}>Logout</button>
+    </header>
+
+    <div id="body-container">
+      {basketOpen == false ?  pizzas && pizzas.map((pizza) => (
+          <div class="pizza">
+          <img src={pizza.image} alt={pizza.image}/>
+          <div class="pizza-name">{pizza.name}</div>
+          <div class="pizza-ingredients">{pizza.ingredients}</div>
+          <div class="pizza-price">{pizza.price}</div>
+          <button onClick={() => addToBasket(pizza)}>Add to basket</button>
+      </div>)) : basket && basket.map((pizza) => (
+        <div class="pizza">
+        <img src={pizza.image} alt={pizza.image}/>
+        <div class="pizza-name">{pizza.name}</div>
+        <div class="pizza-ingredients">{pizza.ingredients}</div>
+        <div class="pizza-price">{pizza.price}</div>
+        <button onClick={() => remo(pizza)}>Remove from basket</button>
+    </div>))}
+       
+    </div>
+      <footer>
+              
+        <div id="footer">
+            <div id ="tel">
+                <p>Tel☎: 06 70 333 9993</p>
+            </div>
+            <div id = "email">
+                <p>@Email:pizzaria@afp.com</p>
+            </div>
+          </div>
+    </footer>
+    </div>
   )
 }
