9cf26dc1320b09231b713e891e06045686a22f8e
Kiliman-Jaro <tothkilian777@gmail.com>
2023-12-13 17:29:46 +0100

    jelszó validálása
    Co-Authored-By: Bóta Dániel <145868940+Danszi44@users.noreply.github.com>
    Co-Authored-By: Bendeguz6666 <93760729+Bendeguz6666@users.noreply.github.com>

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -7,21 +7,34 @@ const RegisterForm = () => {
     const [password, setPassword] = useState("");   
     const [email, setEmail] = useState("");
     const navigate = useNavigate();
+
     const validateEmail = (email) => {
-        // Regular expression for basic email validation
+        
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
       };
 
+    const validatePassword = (password) => {
+        
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).+$/;
+        return passwordRegex.test(password);
+      };
     const handleRegister = () => {
         if(!username || !password || !email) {
             alert("All fields must be filled in!");
             return;
         }
+
         if(!validateEmail(email)) {
             alert("Invalid email format!");
             return;
         }
+
+        if (!validatePassword(password)) {
+            alert("Password must contain at least one uppercase letter and one digit!");
+            return;
+          }
+          
         axios.post("http://localhost:5000/register", {
             username: username,
             password: password,
